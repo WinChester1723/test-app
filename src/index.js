01d598b14324cc8a5f6ca9fbe396ccdcf729e6c8
Pasha Pashazade | Novum LLC <pasha.pashazade@novum.tech>
2023-07-28 19:27:06 +0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const Book = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>{props.year}</p>
+      <p>{props.price}</p>
+    </div>
+  );
+
+  // React.createElement("div", {}, [
+  //   React.createElement("h2", {}, props.name),
+  //   React.createElement("p", {}, props.year),
+  //   React.createElement("p", {}, props.price),
+  // ]);
+};
+
+const App = () => {
+  return (
+    <div>
+      <Book name="Jhonny" year="2018" price="1000$" />
+      <Book name="React" year="2020" price="987$" />
+      <Book name="Vue JS" year="2023" price="969$" />
+    </div>
+  );
+
+  // React.createElement("div", {}, [
+  //   React.createElement(
+  //     "h1",
+  //     { id: "hello", className: "class1" },
+  //     "Hello from React"
+  //   ),
+  //   React.createElement(Book, { name: "Jhonny", year: 2018, price: "1000$" }),
+  //   React.createElement(Book, { name: "React", year: 2020, price: "987$" }),
+  //   React.createElement(Book, { name: "Vue JS", year: 2023, price: "969$" }),
+  // ]);
+};
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
